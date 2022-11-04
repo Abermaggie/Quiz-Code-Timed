0334748f0580687083a7ded5cc1378d9e401a0b6
Abermaggie <114010430+Abermaggie@users.noreply.github.com>
2022-11-04 13:32:30 -0500

    HTML code written.  Just the skeleton with generalized classes.

---
+++ assets/JS/script.js
@@ -0,0 +1,17 @@
+// identify variables you need to use from the HTML id's and classes.
+// You will need to create child variables that allow for list items (lesson 8) that will change for each question.
+// Create a timer in top right corner (lesson10) 
+//  // Create an event handler that when wrong answer is clicked, it decrements time from the timer. (lesson 12)
+// Create a keyboard event that displays a value of right or wrong when the user answers (lesson16)
+// Look at lesson 18 to see if it will help with the forward movement of the questions once they have been answered.
+// Use localstorage to record scores of correct vs. incorrect answers and tally at the end. (lesson 22) (lesson 24)
+// Create form for user to input their initials at the end of the questions and it saves it. (lesson 22) (lesson 25)
+// Questions:
+// // Write a loop function that will insert answers  then remove them after question moves on.
+// high scores link in top left.
+
+
+
+// style:
+// // create responsive buttons that change when hovered over.
+
